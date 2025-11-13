96f0766d76d40fb8c419f5fbb02dc07c7d54d1e8
Mdsanta <madesanta08@gmail.com>
2025-11-13 17:41:54 +0800

    Add API handler for new booking endpoint

---
+++ api/onNewBooking.js
@@ -0,0 +1,6 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Only POST allowed' });
+  }
+  return res.status(200).json({ success: true, message: 'Vercel API active!' });
+}
